f996734686f5a1ef261a8d5ee434cab606aaea22
odelolajosh <oladapoodelola@gmail.com>
2022-05-31 10:31:19 +0100

    [Feature] - Add task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for tens in range(0, 9):
+  for units in range(tens + 1, 10):
+    if (tens == 8 and units == 9):
+      print(f'{tens}{units}')
+    else:
+      print(f'{tens}{units},', end=' ')

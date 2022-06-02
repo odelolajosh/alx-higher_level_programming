59ab650e0126d264945f26d522f950caecc6cee8
odelolajosh <oladapoodelola@gmail.com>
2022-06-02 21:44:39 +0100

    [Fix] - handle plural case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 if __name__ == '__main__':
     import sys
     count = len(sys.argv) - 1
-    s = ':' if count > 0 else '.'
-    print('{} arguments{}'.format(count, s))
+    plural = 's' if count != 1 else ''
+    sign = ':' if count > 0 else '.'
+    print('{} argument{}{}'.format(count, plural, sign))
     for i in range(1, count + 1):
         print('{}: {}'.format(i, sys.argv[i]))

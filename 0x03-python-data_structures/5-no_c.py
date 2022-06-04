f0fa967276b24c12a3b00b79e12a44dc22aaf0cb
odelolajosh <oladapoodelola@gmail.com>
2022-06-04 18:16:32 +0100

    [Fix] - add counter for removed character

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,9 @@
 def no_c(my_string):
     clone = my_string[:]
 
+    j = 0
     for i, ch in enumerate(clone):
         if ch == 'c' or ch == 'C':
-            clone = clone[:i] + clone[i + 1:]
-    return clone;
+            clone = clone[:i - j] + clone[i - j + 1:]
+            j += 1
+    return clone

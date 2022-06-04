7ff4bbb42fb509472ef691dbd5135307483bfaa3
odelolajosh <oladapoodelola@gmail.com>
2022-06-04 17:57:08 +0100

    [Fix] - modify condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     clone = my_list[:]
 
-    if idx >= 0 or idx < len(clone):
+    if 0 <= idx < len(clone):
         clone[idx] = element
 
     return clone

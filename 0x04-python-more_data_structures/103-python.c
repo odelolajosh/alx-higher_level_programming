3a15184bed60c8e1b93f014698c9a867026b9c40
odelolajosh <oladapoodelola@gmail.com>
2022-06-08 21:23:46 +0100

    [Feature] - Advanced Task 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -59,7 +59,7 @@ void print_python_list(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		item = ((pyListobject *)p)->ob_item[i];
+		item = ((PyListObject *)p)->ob_item[i];
 		printf("Element %ld: %s\n", i, ((item)->ob_type)->tp_name);
 		if (PyBytes_Check(item))
 			print_python_bytes(item);

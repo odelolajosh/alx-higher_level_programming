ccca8534f02135b0916cb2ae3b99da39497b3da0
odelolajosh <oladapoodelola@gmail.com>
2022-06-08 17:41:05 +0100

    [Feature] - Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary
+
+    Args:
+        a_dictionary: given dictionary
+
+    Return:
+        length of the key list
+    """
+    return len(a_dictionary.keys())

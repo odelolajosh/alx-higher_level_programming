9c26eb240b89397655fe095966ef4b428fcb169c
odelolajosh <oladapoodelola@gmail.com>
2022-06-08 18:56:57 +0100

    [Feature] - Advanced Task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """
+        Deletes keys with a specific value in a dictionary.
+
+        Args:
+            a_dictionary: Given dictionary
+            value: value to get rid of
+
+        Return:
+            Modified dictionary
+    """
+    keys = list(a_dictionary.keys());
+
+    for key in keys:
+        if value == a_dictionary.get(key):
+            del a_dictionary[key]
+
+    return a_dictionary

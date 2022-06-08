1a21fc3077e9a3e7d7aebc15aaa205f06470301a
odelolajosh <oladapoodelola@gmail.com>
2022-06-08 17:14:13 +0100

    [Chore] - Add documentation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element by another in a new list
+
+    Args:
+        my_list: given list
+        search: elements to replace in the list
+        replace: the new element
+
+    Return:
+        new list
+    """
     return [replace if m is search else m for m in my_list]

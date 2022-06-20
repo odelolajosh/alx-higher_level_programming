6aedec1dbbde3edd1d61ae397a4341f589faf251
odelolajosh <oladapoodelola@gmail.com>
2022-06-20 15:10:01 +0100

    [Feature] - Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Print an integer
+    Args:
+        value: value to print
+    Return:
+        True if value has been correctly printed
+        False if otherwise
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
+
+    return True

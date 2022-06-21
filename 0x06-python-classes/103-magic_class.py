916a7c5a2668fc7f727839a88eb53ed4e66668d6
odelolajosh <oladapoodelola@gmail.com>
2022-06-21 22:37:57 +0100

    [Chore] - refactor docs

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Square Class Module
+Magic Class Module
 """
 
 import math
 
 
 class MagicClass:
-    """
-    Magic class
-    """
+    """Magic class"""
     def __init__(self, radius):
+        """Initializes a Magic object"""
         self.__radius = 0
         if type(radius) is not int:
             if type(radius) is not float:
@@ -18,7 +17,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        return math.pi * self.__radius ** 2
+        """Calculates the area of the circle"""
+        return math.pi * (self.__radius ** 2)
 
     def circumference(self):
+        """Calculates the circumference of the circle"""
         return 2 * math.pi * self.__radius

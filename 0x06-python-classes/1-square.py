80d041413276477ba11c6a5a4ad46da963b4a4b6
odelolajosh <oladapoodelola@gmail.com>
2022-06-21 16:37:37 +0100

    [Feature] - Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Square Class Module
+"""
+class Square:
+	"""Defines a square
+	"""
+	def __init__(self, size):
+		"""Initializes a Square object
+		"""
+		self.__size = size

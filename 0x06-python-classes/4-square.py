e118e86537ece5892f91584b8a95d85e4896a385
odelolajosh <oladapoodelola@gmail.com>
2022-06-21 19:45:37 +0100

    [Chore] - Refactor code to use setter in initializer

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,11 +10,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize the square object
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):

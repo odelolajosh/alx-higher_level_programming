ed9a1b474a33ee81f4df77402fbb2276944cd866
odelolajosh <oladapoodelola@gmail.com>
2022-06-21 17:04:35 +0100

    [Fix] - Fix PEP8 warnings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,16 @@
 """
 Square Class Module
 """
+
+
 class Square:
-	"""Defines a square
-	"""
-	def __init__(self, size=0):
-		"""Initializes a Square object
-		"""
-		if type(size) is not int:
-			raise TypeError('size must be an integer')
-		if size < 0:
-			raise ValueError('size must be >= 0')
-		self.__size = size
+    """Defines a square
+    """
+    def __init__(self, size=0):
+        """Initializes a Square object
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size

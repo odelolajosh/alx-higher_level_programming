8652e21c56f995c157e08f545887368733f40b2e
odelolajosh <oladapoodelola@gmail.com>
2022-06-28 00:25:57 +0100

    [Feature] - Advanced Task 101

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+"""Solves N Queen Problem
+"""
+
+
+def print_queens(solutions=[]):
+    """Prints the solutions
+
+    Args:
+        solutions (list): List of list of position of the queens
+    """
+    for board in solutions:
+        print([[i, x] for i, x in enumerate(board)])
+
+
+def is_safe(queens, row, column):
+    """Checks if the queen can be placed in a position
+
+    Args:
+        queens (list): Chess board with queens positions
+        row (int): row position
+        column (int): column position
+
+    Returns:
+        True: if the queen can be placed at (row, column)
+        False: If the queen can't be placed at (row, column)
+    """
+    for i in range(1, row + 1):
+        # Check Column
+        if queens[row - i] == column:
+            return False
+
+        # Check upleft diagonal
+        if queens[row - i] == column - i:
+            return False
+
+        # Check upright diagonal
+        if queens[row - i] == column + i:
+            return False
+    return True
+
+
+def find_position(queens, k, N):
+    """Find a safe column position for a queen
+
+    Args:
+        queens (list): N x N chess board with queens positions
+        k (int): row position
+        N (int): size of the chess board
+
+    Returns:
+        (int) Safe column if found else -1
+    """
+    for j in range(queens[k] + 1, N):
+        if is_safe(queens, k, j):
+            return j
+    return -1
+
+
+def solve_queens(N, start=-1):
+    """Returns a single solution for N Queen given a start index
+    for the first queen
+
+    Args:
+        N (int): size of the chess board
+        start (int): initial column value for queen at row 0
+
+    Return:
+        (list): a solution
+    """
+    queens = [-1 for i in range(N)]
+    queens[0] = start
+    k = 0
+    while k >= 0 and k < N:
+        j = find_position(queens, k, N)
+        if j < 0:
+            queens[k] = -1
+            k -= 1
+        else:
+            queens[k] = j
+            k += 1
+
+    if k == -1:
+        return None
+
+    return queens
+
+
+def nqueens(N):
+    """Print every possible solution to the given N problem
+
+    Args:
+        N (int): size of chessboard
+    """
+    solutions = []
+    i = -1
+    while i < N - 1:
+        queen = solve_queens(N, i)
+        if queen:
+            i = queen[0]
+            solutions.append(queen)
+        else:
+            i += 1
+    print_queens(solutions)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(N)

e8273f0bedaa876e6e03fb1c3fab44a05954df10
odelolajosh <oladapoodelola@gmail.com>
2022-06-28 21:48:25 +0100

    [Fix] - Add corrections

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
 Locked Class:
-Attributes can't be dynamically created except 'first_name'
+    Attributes can't be dynamically created except 'first_name'
 """
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    """Locked Class
+    """
+    __slots__ = ["first_name"]
 
     def __init__(self):
         """Initializing LockedClass object

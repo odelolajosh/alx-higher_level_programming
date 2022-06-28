3ab976786919929701ff03008d05ce0ecb97c476
odelolajosh <oladapoodelola@gmail.com>
2022-06-28 21:43:44 +0100

    [Fix] - Add corrections

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Locked Class:
-    Attributes can't be dynamically created except 'first_name'
+"""
+Locked Class:
+Attributes can't be dynamically created except 'first_name'
 """
 
 

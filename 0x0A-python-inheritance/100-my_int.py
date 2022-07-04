38384bfd4c94774edce609b91f7ca8e0a629d9a8
odelolajosh <oladapoodelola@gmail.com>
2022-07-04 23:56:50 +0100

    [Feature] - task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,10 +8,10 @@ class MyInt(int):
     """A subclass of `int` object
     """
 
-    def __eq__(self, __x):
+    def __eq__(self, other):
         """ make == comparison """
-        return super().__eq__(__x)
+        return super().__eq__(other)
 
-    def __ne__(self, __x):
+    def __ne__(self, other):
         """ make != comparison """
-        return super().__ne__(__x)
+        return super().__ne__(other)

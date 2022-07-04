82e34dbb6fcec9d814629166df953da7ea7ffa98
odelolajosh <oladapoodelola@gmail.com>
2022-07-05 00:14:18 +0100

    [Fix] - rename `print_sort` to `print_sorted`

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,7 @@
 class MyList(list):
     """A subclass of `list` object
     """
-    def print_sort(self):
+    def print_sorted(self):
         """prints the list, but sorted (ascending sort)
         """
         print(sorted(self))

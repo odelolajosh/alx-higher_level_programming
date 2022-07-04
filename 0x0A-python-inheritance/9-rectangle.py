7bb1f5cdfa08ec6c7ff4482908f05029d5f20b59
odelolajosh <oladapoodelola@gmail.com>
2022-07-04 23:40:42 +0100

    [Fix] - remove typo errors

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle(BaseGeometry):
             height (int): The height of the rectangle
         """
         self.integer_validator("width", width)
-        self.integer_validator("heiaght", height)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 

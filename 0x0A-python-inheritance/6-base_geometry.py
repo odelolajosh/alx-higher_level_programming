0baf30678faed4fcfcf4c05d362f1a03d46a8988
odelolajosh <oladapoodelola@gmail.com>
2022-07-04 22:34:41 +0100

    [Feature] - task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Base Geometry class
+"""
+
+
+class BaseGeometry:
+    """An empty Geometry class
+    """
+    def area(self):
+        """Base area implementation
+        """
+        raise Exception("area() is not implemented")

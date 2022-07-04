59cac7273ae3eaaddce7632cfc01a09e07f39a3d
odelolajosh <oladapoodelola@gmail.com>
2022-07-04 23:49:54 +0100

    [Feature] - task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Square class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class from Rectangle class
+    """
+
+    def __init__(self, size):
+        """Instantiates Square
+
+        Args:
+            size (int): The length of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Calculates area """
+        return super().area()
+
+    def __str__(self):
+        """ Returns printable string representation for Rectangle """
+        return super().__str__()

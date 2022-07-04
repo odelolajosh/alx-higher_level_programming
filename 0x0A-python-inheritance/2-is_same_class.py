6052c6f9d7cf5db61e03935e3ad04e98bad24af9
odelolajosh <oladapoodelola@gmail.com>
2022-07-04 22:02:51 +0100

    [Feature] - task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """checks if an object is exactly an instance of the specified class
+
+    Args:
+        obj: given object
+        a_class: given class
+
+    Return:
+        return True is obj is exactly an instance of a_class else False
+    """
+    return type(obj) is a_class

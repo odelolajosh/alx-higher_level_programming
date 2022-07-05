6262b7d829d85a67c4d9651786139e6f99cdf049
odelolajosh <oladapoodelola@gmail.com>
2022-07-05 22:18:02 +0100

    [Feature] - task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+4-from_json_string Module
+"""
+import json
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure) represented by a JSON string
+
+    Args:
+        my_obj (string): given json
+
+    Return:
+        (string) string representation
+    """
+    return json.loads(my_str)

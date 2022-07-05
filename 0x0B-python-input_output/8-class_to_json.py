c4aa5d0b22db186cef5fbf37fc1e043ace186e55
odelolajosh <oladapoodelola@gmail.com>
2022-07-05 22:51:00 +0100

    [Feature] - task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+8-class_to_json Module
+"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization
+    of an object
+
+    Args:
+        obj (object): given object
+    """
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__.copy()
+    return {}

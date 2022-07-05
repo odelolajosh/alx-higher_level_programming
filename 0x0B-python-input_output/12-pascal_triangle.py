548376b1b8fcd9a0ceae0ec5dfc632c15a060c47
odelolajosh <oladapoodelola@gmail.com>
2022-07-06 00:31:38 +0100

    [Feature] - task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+12-pascal_triangle Module
+    Contains a function that returns a list of list of integers representing
+    the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal's triangle of n
+
+    Args:
+        n (int): order of the triangle
+
+    Return:
+        (list): list representation of pascal triangle
+    """
+    triangle = []
+
+    if n <= 0:
+        return []
+
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+            continue
+
+        row = []
+        prev = [0] + triangle[i - 1] + [0]
+
+        runningSum = 0
+        for i in range(len(prev)):
+            runningSum += prev[i]
+
+            if i >= 1:
+                row.append(runningSum)
+                runningSum -= prev[i - 1]
+
+        triangle.append(row)
+    return triangle

95f6e39bbba38ddf9442661bd3e060aa66168baf
odelolajosh <oladapoodelola@gmail.com>
2022-08-10 00:18:14 +0100

    [Fix] - create table if not exist

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,11 @@
 -- creates a table second_table in the database hbtn_0c_0
 -- in your MySQL server and add multiples rows.
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
+
 -- perform insertion
 INSERT INTO second_table (id, name, score) VALUES
     ROW(1, "John", 10), ROW(2, "Alex", 3),

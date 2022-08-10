21f79bad54505abb4ad3df15f50240721f4a64cf
odelolajosh <oladapoodelola@gmail.com>
2022-08-10 22:14:09 +0100

    [Feature] - task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT
-	c.id as id,
-	c.name as name,
-	s.name as name
+SELECT c.id, c.name, s.name
 FROM cities c
 JOIN states s ON c.state_id = s.id;
 

4e54bdb700a30ba11c0162531f53154d4ae174ea
odelolajosh <oladapoodelola@gmail.com>
2022-08-10 01:13:52 +0100

    [Fix] - add fixes

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- lists the number of records with the same score in the table second_table
 -- of the database hbtn_0c_0 in your MySQL server.
-SELECT DISTINCT score COUNT(*) AS number
+SELECT score COUNT(*) AS number
+    GROUP BY score
     ORDER BY score DESC;

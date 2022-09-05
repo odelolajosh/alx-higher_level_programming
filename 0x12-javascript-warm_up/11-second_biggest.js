31d04fdf91e7b9e9c7260ab2abce37316b8bc8d0
odelolajosh <oladapoodelola@gmail.com>
2022-09-05 21:01:27 +0100

    fix: make a number array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const argv = process.argv.slice(2);
+const args = argv.map(arg => parseInt(arg))
 let biggest = 0;
 
 if (args.length > 1) {
   biggest = Math.max(...args);
-  const index = args.indexOf(biggest + '');
+  const index = args.indexOf(biggest);
   args.splice(index);
   biggest = Math.max(...args);
 }

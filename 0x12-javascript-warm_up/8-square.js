f8e594a1e13ccd392460e9166c57500601198805
odelolajosh <oladapoodelola@gmail.com>
2022-09-05 20:15:35 +0100

    fix: remove use of array

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-let size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
   process.exit(0);
 }
 
-const row = new Array(size).fill('X').join('');
+let s = '';
+for (let i = 0; i < size; i++) {
+  s += 'X';
+}
 
-for (let i = 0; i < parseInt(size); i++) {
-  console.log(row);
+for (let i = 0; i < size; i++) {
+  console.log(s);
 }

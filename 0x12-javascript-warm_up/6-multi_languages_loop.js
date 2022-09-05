5ac839ec4b1a0d66cb31486d2dae85da0f917156
odelolajosh <oladapoodelola@gmail.com>
2022-09-05 19:23:09 +0100

    fix: use raw for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const lang of langs) {
-  console.log(lang);
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
 }

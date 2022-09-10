05e048814dc2e2926fd1740af8da77c613f0af0b
odelolajosh <oladapoodelola@gmail.com>
2022-09-10 12:54:32 +0100

    fix: use for loop

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -29,6 +29,5 @@ if __name__ == "__main__":
 
     states = session.query(State).order_by(State.id).all()
 
-    print("\n".join(
-        ["{}: {}".format(state.id, state.name) for state in states]
-    ))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))

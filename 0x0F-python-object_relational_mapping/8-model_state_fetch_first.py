82237c2d8e51190e2af9234844046ffc9df39e32
odelolajosh <oladapoodelola@gmail.com>
2022-09-10 12:56:06 +0100

    fix: display \`nothing\` on empty table

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -29,4 +29,7 @@ if __name__ == "__main__":
 
     state = session.query(State).order_by(State.id).first()
 
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print('Nothing')

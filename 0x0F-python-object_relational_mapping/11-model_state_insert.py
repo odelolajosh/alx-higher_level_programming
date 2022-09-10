617994290537451a88c8bf664a8fc6309637168c
odelolajosh <oladapoodelola@gmail.com>
2022-09-10 22:49:58 +0100

    feature: task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+lists all `State` objects that contain the letter a
+"""
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+host = "localhost"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("USAGE: {} user passwd database".format(sys.argv[0]))
+        sys.exit(1)
+
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@{}/{}'.format(user, passwd, host, db),
+        pool_pre_ping=True
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    new = State(name='Louisiana')
+    session.add(new)
+    session.commit()
+
+    print(new.id)

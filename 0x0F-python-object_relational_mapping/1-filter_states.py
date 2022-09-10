efae6cdd5781818776d9c0b549b2bc0e6355592e
odelolajosh <oladapoodelola@gmail.com>
2022-09-10 02:06:30 +0100

    fix: remove with contextI dont think MySQLdb supports with in python

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,18 +2,23 @@
 import sys
 import MySQLdb
 
-if len(sys.argv) != 4:
-    print("USAGE: ./0-select_states.py user passwd database")
-    sys.exit(1)
-
 host = "localhost"
 port = 3306
-user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
 
-with MySQLdb.connect(host=host, user=user, passwd=passwd, db=db) as db:
-    with db.cursor() as cur:
-        query = """SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC"""
-        cur.execute(query)
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("USAGE: ./0-select_states.py user passwd database")
+        sys.exit(1)
+
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+    cur = db.cursor()
+    query = """SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC"""
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()

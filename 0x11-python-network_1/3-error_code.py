b526a6d01241a0e108351370590d74bc44bafb9b
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 21:03:41 +0100

    feat: add task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL and displays the body of the
+response handling error appropriately
+"""
+from urllib import request, error as urlerror
+import sys
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().encode("utf8"))
+    except urlerror.HTTPError as e:
+        print("Error code: {}".format(e.code))

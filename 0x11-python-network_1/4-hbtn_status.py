a7b49e53adc3039bfaa5a36a4982f566aec9d695
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 21:19:08 +0100

    feat: add task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     body = response.text
-    print("Body response: ")
+    print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))

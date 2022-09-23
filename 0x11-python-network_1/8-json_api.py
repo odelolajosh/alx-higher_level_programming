b8efcfa7b485eb3587bcccc062dcda795a06f90d
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 22:06:52 +0100

    feat: add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    q = sys.argv[0] if len(sys.argv) >= 1 else ""
+    params = {'q': q}
+    r = requests.post("http://0.0.0.0:5000/search_user", params=params)
+
+    try:
+        json = r.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")

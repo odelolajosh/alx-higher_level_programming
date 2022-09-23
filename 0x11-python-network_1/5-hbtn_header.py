2de4d71435576693db11fac9b78179931c40420a
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 21:36:36 +0100

    feat: add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Takes a URL and retrieve the header value of the X-Request-Id """
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print(response.headers["X-Request-Id"])

67ddd059f84ad471d633e07ff2680734dd7acaf1
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 21:42:42 +0100

    fix: add fixes

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Takes a URL and retrieve the header value of the X-Request-Id """
 import requests
+import sys
 
 if __name__ == "__main__":
-    response = requests.get("https://alx-intranet.hbtn.io/status")
+    response = requests.get(sys.argv[1])
     print(response.headers.get("X-Request-Id"))

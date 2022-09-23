1cbb3839f33c7c20a302bf5004a91176f3a0a392
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 20:46:02 +0100

    feat: add task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL """
+from urllib import request, parse
+import sys
+
+values = {"email": sys.argv[2]}
+data = parse.urlencode(values).encode("ascii")
+req = request.Request(sys.argv[1], data)
+with request.urlopen(req) as response:
+    print(response.read().decode("utf8"))

bb3215d87003e8f31ac7ab64211fd8c1803e10fc
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 22:12:14 +0100

    fix: add fixes

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,14 +7,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    q = sys.argv[1] if len(sys.argv) > 0 else ""
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     params = {'q': q}
     r = requests.post("http://0.0.0.0:5000/search_user", params=params)
 
     try:
         json = r.json()
         if json:
-            print("[{}] {}".format(json.get("id"), json.get("name")))
+            print("[{}] <{}>".format(json.get("id"), json.get("name")))
         else:
             print("No result")
     except requests.exceptions.JSONDecodeError:

e9fd86a2adb148413218701a024a0089daf87045
odelolajosh <oladapoodelola@gmail.com>
2022-09-23 20:27:23 +0100

    feat: add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Fetches `https://alx-intranet.hbtn.io/status` """
+from urllib import request
+
+with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body: bytes = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf8")))

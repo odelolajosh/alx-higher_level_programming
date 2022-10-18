b131136fed6b0ad1e19a7d8f51f43afd92ea6e54
odelolajosh <oladapoodelola@gmail.com>
2022-10-18 23:08:17 +0100

    feat: get status of a url

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const request = require('request');
+
+const url = process.argv[2];
+request(url, function (error, response) {
+    console.log(`code: ${response.statusCode}`);
+})

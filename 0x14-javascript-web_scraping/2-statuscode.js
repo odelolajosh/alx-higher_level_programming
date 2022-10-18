7aeb0eb896d7e560281b824ea4f9a18fc0315b0d
odelolajosh <oladapoodelola@gmail.com>
2022-10-18 23:19:13 +0100

    chore: fix lint

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,9 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log(`code: ${response.statusCode}`);
 });

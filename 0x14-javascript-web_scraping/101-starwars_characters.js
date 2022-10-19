fdb9d64fc35a0d3d730a9f1903d88cbe8ad1ca59
odelolajosh <oladapoodelola@gmail.com>
2022-10-19 19:24:13 +0100

    fix: lint

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-function getCharacterByFilm(url) {
+function getCharacterByFilm (url) {
   request(url, function (err, response, body) {
     if (err) {
       console.log(err);
@@ -18,9 +18,9 @@ function getCharacterByFilm(url) {
       }
     });
     if (body.next) {
-      getCharacterByFilm(body.next)
+      getCharacterByFilm(body.next);
     }
-  })
+  });
 }
 
-getCharacterByFilm('https://swapi-api.hbtn.io/api/people/')
+getCharacterByFilm('https://swapi-api.hbtn.io/api/people/');
